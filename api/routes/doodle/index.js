6aaa5995d122d0b2b54c4e6f85ee9292bf12beb3
Richard Kaufman <richardbkaufman@gmail.com>
2015-08-27 23:01:38 -0500

    GET /api/doodle/:date

--- api/routes/doodle/index.js
+++ api/routes/doodle/index.js
@@ -1,5 +1,6 @@
 import {Router} from 'express'
 import {Doodle} from '../../models'
+import moment from 'moment-timezone'
 
 const doodleRoutes = Router()
 
@@ -11,11 +12,11 @@ doodleRoutes.route('/')
         return res.sendStatus(500)
       }
 
-      res.json(doodles)
+      return res.json(doodles)
     })
   })
   .post(function (req, res) {
-    // TODO: Clean, parse, escape, Doodle properties.
+    // TODO: Sanitize input.
     const {date, image, alt, url} = req.body
     const doodle = new Doodle({
       date,
@@ -31,13 +32,33 @@ doodleRoutes.route('/')
       }
 
       console.log(`Saved Doodle to database.`)
-      res.sendStatus(201)
+      return res.sendStatus(201)
     })
   })
 
 doodleRoutes.route('/:date')
   .get(function (req, res) {
-    res.sendStatus(501)
+    // TODO: Sanitize input.
+    const date = moment(req.params.date)
+    const year = date.year()
+    const month = date.month()
+    const day = date.date()
+
+    const doodleDate = moment.tz(`${year}-${month+1}-${day}`, 'YYYY-MM-DD', 'UTC')
+
+    Doodle.findOne({
+      date: {
+        $gte: doodleDate.startOf('day').clone(),
+        $lte: doodleDate.endOf('day').clone()
+      }
+    }, function (error, doodle) {
+      if (error) {
+        console.error(`Error fetching doodle`)
+        return res.sendStatus(500)
+      }
+
+      return res.json(doodle)
+    })
   })
   .put(function (req, res) {
     res.sendStatus(501)

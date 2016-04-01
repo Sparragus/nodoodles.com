824037248b0bdbd6aa9446fa5b86a5edfad61a6f
Richard Kaufman <richardbkaufman@gmail.com>
2016-04-01 13:40:33 -0600

    Adds hello world server

---
+++ server/index.js
@@ -0,0 +1,17 @@
+import 'babel-polyfill'
+
+const Koa = require('koa')
+const app = new Koa()
+
+app.use(async function (ctx, next) {
+  const start = new Date()
+  await next()
+  const ms = new Date() - start
+  console.log(`${ctx.method} ${ctx.url} - ${ms}ms`)
+})
+
+app.use((ctx) => {
+  ctx.body = 'Hello Koa'
+})
+
+app.listen(3000)

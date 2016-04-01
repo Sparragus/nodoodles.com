6260af55b23f8f5d0af0f15326d160346e5e0596
Richard Kaufman <richardbkaufman@gmail.com>
2016-04-01 13:56:27 -0600

    Adds router

--- server/index.js
+++ server/index.js
@@ -3,14 +3,14 @@ import 'babel-polyfill'
 import Koa from 'koa'
 import logger from 'koa-logger'
 
+import routes from './routes'
+
 const app = new Koa()
 
 if (process.env.NODE_ENV === 'development') {
   app.use(logger())
 }
 
-app.use((ctx) => {
-  ctx.body = 'Hello Koa'
-})
+app.use(routes())
 
 app.listen(3000)
---
+++ server/routes/index.js
@@ -0,0 +1,20 @@
+import Router from 'koa-router'
+import compose from 'koa-compose'
+
+export default function () {
+  const publ = new Router()
+  const priv = new Router()
+
+  publ.get('*', async function (ctx, next) {
+    ctx.body = 'Hello, World!'
+    await next()
+  })
+
+  return compose([
+    publ.routes(),
+    publ.allowedMethods(),
+    // auth(),
+    priv.routes(),
+    priv.allowedMethods()
+  ])
+}

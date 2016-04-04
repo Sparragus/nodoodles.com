a4a427b278c46265b4032835c58c906fb70139dd
Richard Kaufman <richardbkaufman@gmail.com>
2016-04-04 17:46:32 -0500

    Add login and signup routes

--- server/api/index.js
+++ server/api/index.js
@@ -3,20 +3,33 @@ import compose from 'koa-compose'
 
 import auth from '../middleware/auth'
 
+// Routes
+import signup from './users/signup'
+import login from './users/login'
+
+function noop (ctx, next) { return }
+
 export default function () {
   const publ = new Router()
   const priv = new Router()
 
-  publ.get('*', async function (ctx, next) {
-    ctx.body = 'Hello, World!'
-    await next()
-  })
+  // publ.get('*', async function (ctx, next) {
+  //   ctx.body = 'Hello, World!'
+  //   await next()
+  // })
 
+  // Public Routes
+  publ.get('/doodle', noop) // Get today's doodle(s)
+  publ.post('/signup', signup) // Sign up
+  publ.post('/login', login) // Log in
+
+  // Private Routes
+  
   return compose([
     publ.routes(),
     publ.allowedMethods(),
-    auth(),
     priv.routes(),
-    priv.allowedMethods()
+    priv.allowedMethods(),
+    auth()
   ])
 }

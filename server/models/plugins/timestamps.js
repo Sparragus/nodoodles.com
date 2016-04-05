62318b91d9c1dff1dd1db8d2b482be7112ddf4cd
Richard Kaufman <richardbkaufman@gmail.com>
2016-04-05 13:10:32 -0500

    Adds options to transform

--- server/models/plugins/timestamps.js
+++ server/models/plugins/timestamps.js
@@ -1,7 +1,27 @@
-function timestamps (schema, options) {
+import { defaultsDeep } from 'lodash'
+
+function timestamps (schema, options = {}) {
+  const defaultOptions = {
+    createdAt: {
+      name: 'createdAt',
+      options: {
+        type: Date,
+        default: Date.now
+      }
+    },
+    modifiedAt: {
+      name: 'modifiedAt',
+      options: {
+        type: Date
+      }
+    }
+  }
+
+  const {createdAt, modifiedAt} = defaultsDeep({}, defaultOptions, options)
+
   schema.add({
-    createdAt: { type: Date, default: Date.now },
-    modifiedAt: { type: Date }
+    [createdAt.name]: createdAt.options,
+    [modifiedAt.name]: modifiedAt.options
   })
 
   schema.pre('save', function (next) {

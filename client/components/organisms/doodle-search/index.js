7ac20f14fbbed86861ec629edef5ae3a56c87cd3
Richard Kaufman <richardbkaufman@gmail.com>
2016-04-13 18:12:30 -0500

    Adds style to doodle-search

--- client/components/organisms/doodle-search/index.js
+++ client/components/organisms/doodle-search/index.js
@@ -1,22 +1,25 @@
 import React from 'react'
+import './style.css'
 
 function DoodleSearch (props) {
   const { doodle } = props
 
   return (
-    <div className='search'>
-      <a href={doodle.url}>
-        <img
-          className='doodle'
-          src={doodle.image}
-          title={doodle.altText}
-          alt={doodle.altText} />
-      </a>
-      <div className='search-controls'>
-        <input type='text' />
-        <div className='search-buttons'>
-          <input type='submit' value='NoDoodles Search'></input>
-          <input type='submit' value="I'm Feeling Lucky"></input>
+    <div className='container'>
+      <div className='search'>
+        <a href={doodle.url}>
+          <img
+            className='doodle'
+            src={doodle.image}
+            title={doodle.altText}
+            alt={doodle.altText} />
+        </a>
+        <div className='search-controls'>
+          <input type='text' />
+          <div className='search-buttons'>
+            <input type='submit' value='NoDoodles Search'></input>
+            <input type='submit' value="I'm Feeling Lucky"></input>
+          </div>
         </div>
       </div>
     </div>
